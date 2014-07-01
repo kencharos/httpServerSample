af71686829c56d49420e89043126a575888ec471
Kentaro Maeda <many.front@gmail.com>
2014-07-01 13:39:20 +0900

    make go http server

---
+++ go/server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	// args[0] is program name.
+	path := os.Args[1]
+	port := "4000"
+	if len(os.Args) > 2 {
+		port = os.Args[2]
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/html")
+		file, _ := ioutil.ReadFile(path + "/welcom.html")
+
+		fmt.Fprint(w, string(file))
+	})
+
+	http.HandleFunc("/calc", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "text/html")
+		if r.Method == "GET" {
+			file, _ := ioutil.ReadFile(path + "/calc.html")
+			// string is []byte alias
+			fmt.Fprintf(w, string(file))
+		} else {
+			n1 := r.FormValue("n1")
+			n2 := r.FormValue("n2")
+
+			res := "Wrong input"
+			if isNumber(n1) && isNumber(n2) {
+				i1, _ := strconv.Atoi(n1)
+				i2, _ := strconv.Atoi(n2)
+				res = strconv.Itoa(i1 + i2)
+			}
+
+			file, _ := ioutil.ReadFile(path + "/result.html")
+
+			fmt.Fprintf(w, string(file), res)
+
+		}
+	})
+	fmt.Println("Address = localhost:" + port)
+	fmt.Println("path = " + path)
+	err := http.ListenAndServe("localhost:"+port, nil)
+	fmt.Println(err)
+}
+
+func isNumber(input string) bool {
+	if input == "" {
+		return false
+	}
+	ok, _ := regexp.MatchString("^[+-]?[0-9]+$", input)
+	return ok
+
+}
